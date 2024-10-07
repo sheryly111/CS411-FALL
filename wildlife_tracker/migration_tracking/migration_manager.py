3cf951db4aa2c08b3ab02702e0870f62fda5d6bd
sheryly <sheryly@bu.edu>
2024-10-07 19:38:37 -0400

    submitting

---
+++ wildlife_tracker/migration_tracking/migration_manager.py
@@ -0,0 +1,31 @@
+from typing import Optional, Any 
+from wildlife_tracker.migration_tracking.migration import Migration
+
+class MigrationManager:
+    def __init__(self) -> None:
+        migrations: dict[int, Migration] = {}
+
+    def cancel_migration(self, migration_id: int) -> None:
+        pass
+
+    def get_migration_by_id(self, migration_id: int) -> Migration:
+        pass
+
+    def get_migration_details(self, migration_id: int) -> dict[str, Any]:
+        pass
+
+    def get_migrations(self) -> list[Migration]:
+        pass
+
+    def get_migrations_by_current_location(self, current_location: str) -> list[Migration]:
+        pass
+
+    def get_migrations_by_migration_path(self, migration_path_id: int) -> list[Migration]:
+        pass
+
+    def get_migrations_by_start_date(self, start_date: str) -> list[Migration]:
+        pass
+
+    def get_migrations_by_status(self, status: str) -> list[Migration]:
+        pass
+
